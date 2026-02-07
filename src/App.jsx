7dadcda35812818664f7f566493de7ae7bd25d4b
rezafauzan <rezafauzan593@gmail.com>
2026-02-07 21:59:35 +0700

    Feat: add react-hook-form dependencies; add form to add todo item

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react"
 import moment from "moment"
 import { useSelector } from "react-redux"
+import { useForm } from "react-hook-form"
 
 const TodoItem = ({ id, todoName, todoCreatedAt, todoDoneAt, todoTarget }) => {
     const checkboxRef = React.useRef()
@@ -39,6 +40,42 @@ const TodoItem = ({ id, todoName, todoCreatedAt, todoDoneAt, todoTarget }) => {
     )
 }
 
+const AddToDoForm = () => {
+    const [todos, setTodos] = useState(null)
+    const todoItems = useSelector(state => state.todoReducer)
+    useEffect(
+        () => {
+            setTodos(todoItems)
+        }, [todoItems]
+    )
+    const { register, handleSubmit } = useForm()
+    function addTodo(data) {
+        const todo = {
+            id: todos.length,
+            name: data.todoName,
+            target: moment(data.todoTarget).format('DD MMMM YYYY HH:mm'),
+            createdAt: moment().format('DD MMMM YYYY HH:mm'),
+            doneAt: ""
+        }
+        console.log(todo)
+    }
+    return (
+        <form className="rounded bg-white shadow p-4 flex justify-center gap-4" onSubmit={handleSubmit(addTodo)}>
+            <div className="flex justify-center items-center gap-4">
+                <div className="flex-1">
+                    <label htmlFor="todoName">To Do Name :</label>
+                    <input type="text" {...register("todoName")} id="todoName" className="outline-none border-b border-b-slate-400" placeholder="I need to do ..." />
+                </div>
+                <div className="flex-1">
+                    <label htmlFor="todoTarget">When it should be done :</label>
+                    <input type="datetime-local" {...register("todoTarget")} id="todoTarget" className="border-b border-b-slate-400" />
+                </div>
+                <button className="bg-slate-800 hover:bg-slate-400 flex-1 rounded py-4 text-white cursor-pointer">Submit</button>
+            </div>
+        </form>
+    )
+}
+
 function App() {
     const [todos, setTodos] = useState(null)
     const todoItems = useSelector(state => state.todoReducer)
@@ -48,11 +85,12 @@ function App() {
         }, [todoItems]
     )
     return (
-        <section>
-            <div className="container w-3xl p-4 mx-auto my-4 flex flex-col gap-4">
+        <div className="max-w-4xl w-full p-4 mx-auto my-4 flex flex-col gap-4">
+            <section className="flex flex-col gap-4">
                 <div className="section-header rounded bg-white shadow p-4 flex justify-center items-center">
                     <h1 className="text-xl font-bold">ToDo List React App</h1>
                 </div>
+                <AddToDoForm />
                 <div className="section-body rounded bg-white shadow p-4 flex flex-col justify-center items-center gap-4">
                     <h1 className="w-full text-xl font-bold">To Do:</h1>
                     <ul className="w-full flex flex-col gap-4">
@@ -79,8 +117,8 @@ function App() {
                         }
                     </ul>
                 </div>
-            </div>
-        </section >
+            </section >
+        </div>
     )
 }
 
