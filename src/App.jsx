999388337ad57650951804bd339621f39456221f
rezafauzan <rezafauzan593@gmail.com>
2026-02-07 21:32:07 +0700

    Feat: now todo list populated from redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,52 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import moment from "moment"
+import { useSelector } from "react-redux"
 
-const todo = [
-    {
-        id: 1,
-        name: "cuci pakaian",
-        createdAt: "22 januari 2025",
-        doneAt: "24 januari 2025",
-        target: "26 januari 2025",
-    },
-    {
-        id: 2,
-        name: "beli bahan makanan seminggu",
-        createdAt: "22 januari 2025",
-        doneAt: "24 januari 2025",
-        target: "26 januari 2025",
-    },
-    {
-        id: 3,
-        name: "cuci motor",
-        createdAt: "22 januari 2025",
-        doneAt: "24 januari 2025",
-        target: "26 januari 2025",
-    },
-    {
-        id: 4,
-        name: "weekly",
-        createdAt: "22 januari 2025",
-        doneAt: "24 januari 2025",
-        target: "26 januari 2025",
-    }
-]
-
-const TodoItem = () => {
-    const checkbox = React.useRef()
-    const todoName = React.useRef()
-    const todoDone = React.useRef()
+const TodoItem = ({ id, todoName, todoCreatedAt, todoDoneAt, todoTarget }) => {
+    const checkboxRef = React.useRef()
+    const todoNameRef = React.useRef()
+    const todoDoneRef = React.useRef()
     function toggleStatus() {
-        if (checkbox.current.checked) {
-            todoName.current.classList.add('line-through')
-            todoDone.current.textContent = moment().format('DD-MM-YYYY hh:mm:ss')
+        if (checkboxRef.current.checked) {
+            todoNameRef.current.classList.add('line-through')
+            todoDoneRef.current.textContent = moment().format('DD-MM-YYYY hh:mm:ss')
         } else {
-            todoName.current.classList.remove('line-through')
-            todoDone.current.textContent = ''
+            todoNameRef.current.classList.remove('line-through')
+            todoDoneRef.current.textContent = ''
         }
     }
     return (
-        <ul>
-            <label>
-                <div className="flex gap-4 flex-1">
-                    <input ref={checkbox} type="checkbox" id="checkbox" name="todo" className="w-7 h-7" onChange={toggleStatus} />
-                    <span ref={todoName}>Tugas 1</span>
+        <label>
+            <div className="flex gap-4 flex-1">
+                <input ref={checkboxRef} type="checkboxRef" id="checkboxRef" name="todo" className="w-7 h-7" onChange={toggleStatus} />
+                <span ref={todoNameRef}>{todoName}</span>
+            </div>
+            <div className="flex flex-col pl-16 flex-1">
+                <div className="flex">
+                    <span className="font-bold w-50">Tanggal dibuat:</span>
+                    <span className="flex">{todoCreatedAt}</span>
                 </div>
-                <div className="flex flex-col pl-16 flex-1">
-                    <div className="flex">
-                        <span className="font-bold w-50">Tanggal dibuat:</span>
-                        {/* <span ref={todoCreated} className="flex"> 28 Januari 2026</span> */}
-                    </div>
-                    <div className="flex">
-                        <span className="font-bold w-50">Target selesai:</span>
-                        {/* <span ref={todoDeadline} className="flex">02 Februari 2026</span> */}
-                    </div>
-                    <div className="flex">
-                        <span className="font-bold w-50">Selesai pada:</span>
-                        <span ref={todoDone} className="flex">01 Februari 2026</span>
-                    </div>
+                <div className="flex">
+                    <span className="font-bold w-50">Target selesai:</span>
+                    <span className="flex">{todoTarget}</span>
                 </div>
-            </label>
-        </ul>
+                <div className="flex">
+                    <span className="font-bold w-50">Selesai pada:</span>
+                    <span ref={todoDoneRef} className="flex">{todoDoneAt}</span>
+                </div>
+            </div>
+        </label>
     )
 }
 
 function App() {
+    const [todos, setTodos] = useState(null)
+    const todoItems = useSelector(state => state.todoReducer)
+    useEffect(
+        ()=>{
+            setTodos(todoItems)
+        }, [todoItems]
+    )
     return (
         <section>
             <div className="container w-3xl p-4 mx-auto my-4 flex flex-col gap-4">
@@ -81,13 +56,25 @@ function App() {
                 <div className="section-body rounded bg-white shadow p-4 flex flex-col justify-center items-center gap-4">
                     <h1 className="w-full text-xl font-bold">To Do:</h1>
                     <ul className="w-full flex flex-col gap-4">
-                        <li>
-                            <TodoItem />
-                        </li>
+                        {
+                            todos != null
+                                ?
+                                todos.map(
+                                    todo => {
+                                        return (
+                                            <li>
+                                                <TodoItem id={todo.id} todoNameRef={todo.name} todoCreatedAt={todo.createdAt} todoDoneRefAt={todo.doneAt} todoTarget={todo.target} />
+                                            </li>
+                                        )
+                                    }
+                                )
+                                :
+                                ""
+                        }
                     </ul>
                 </div>
             </div>
-        </section>
+        </section >
     )
 }
 
