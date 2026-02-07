abb1bd6edd4b008073e3c83a3b611a49960da127
rezafauzan <rezafauzan593@gmail.com>
2026-02-07 23:02:05 +0700

    Fix: wrong logic to remove todoItems, and state immutable

--- src/redux/reducers/todoReducer.js
+++ src/redux/reducers/todoReducer.js
@@ -2,16 +2,17 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const todoReducer = createSlice({
     name: "ToDo Item",
-    initialState: [],
+    initialState: { todoItems: [] },
     reducers: {
-        addTodoItem(state, action){
-            state.push(action.payload)
+        addTodoItem(state, action) {
+            state.todoItems.push(action.payload)
+            console.log(state.todoItems)
         },
-        removeTodoItem(state, action){
-            state = state.filter(todoItem=>todoItem.id != action.payload.id)
+        removeTodoItem(state, action) {
+            state.todoItems = state.todoItems.filter(todoItem => todoItem.id !== parseInt(action.payload.id))
         }
     }
 })
 
-export const {addTodoItem} = todoReducer.actions
+export const { addTodoItem, removeTodoItem } = todoReducer.actions
 export default todoReducer.reducer
