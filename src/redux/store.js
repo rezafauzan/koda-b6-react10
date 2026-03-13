8d9bb18a1e2e78b5a95674d05a5cd81ce441ef48
rezafauzan <rezafauzan593@gmail.com>
2026-03-13 09:15:29 +0700

    Feat: now reducers and store is persisted

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import reducers from "./index";
+import storage from "redux-persist/lib/storage"
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
 
-const store = configureStore({
-    reducer: reducers
+const config= {
+    key: "todo_item",
+    storage
+}
+
+const persistedReducers = persistReducer(config, reducers)
+
+export const store = configureStore({
+    reducer: persistedReducers,
+    middleware: (getDefaultMiddleware)=>{
+        return getDefaultMiddleware({
+            serializableCheck: false
+        })
+    }
 })
 
-export default store
+export const persistedStore = persistStore(store)
