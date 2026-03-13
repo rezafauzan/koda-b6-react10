9d64d93dd455280d8b7a3367b2312b53341d1232
rezafauzan <rezafauzan593@gmail.com>
2026-03-13 09:17:07 +0700

    Feat: app now wrapped with persistgate

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import store from './redux/store.js'
+import {store, persistedStore} from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistedStore}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>,
 )
